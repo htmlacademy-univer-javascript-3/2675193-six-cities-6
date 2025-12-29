7b2a1133c39edf13a7ae4dfc6c206e36a01bac6a
ilya <ilyarybin2018@gmail.com>
2025-12-30 01:43:11 +0500

    big

--- src/store/redirect-middleware.ts
+++ src/store/redirect-middleware.ts
@@ -2,6 +2,7 @@ import {PayloadAction} from '@reduxjs/toolkit';
 import {Middleware} from 'redux';
 import browserHistory from '../browser-history.ts';
 import {rootReducer} from './root-reducer.ts';
+import {AppRoute} from '../const.ts';
 
 type Reducer = ReturnType<typeof rootReducer>;
 
@@ -9,8 +10,8 @@ export const redirect: Middleware<unknown, Reducer> =
   () =>
     (next) =>
       (action: PayloadAction<string>) => {
-        if (action.type === 'game/redirectToRoute') {
-          browserHistory.push(action.payload);
+        if (action.type === 'user/login') {
+          browserHistory.push(AppRoute.Root);
         }
 
         return next(action);
